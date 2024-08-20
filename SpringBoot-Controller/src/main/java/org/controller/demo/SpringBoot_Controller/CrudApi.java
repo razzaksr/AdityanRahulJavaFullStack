3be2b5c6129cc439109353d5a6e35a3186a6a0b4
razzaksr <razzaksr@gmail.com>
2024-08-20 21:12:21 +0530

    Spring boot web CRUD using collection done with client

--- SpringBoot-Controller/src/main/java/org/controller/demo/SpringBoot_Controller/CrudApi.java
+++ SpringBoot-Controller/src/main/java/org/controller/demo/SpringBoot_Controller/CrudApi.java
@@ -1,9 +1,6 @@
 package org.controller.demo.SpringBoot_Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +10,8 @@ import java.util.stream.Stream;
 @RestController
 @RequestMapping("/card")
 public class CrudApi {
+
+    // collection as storage logic: temporary memory
     private List<Creditcard> myBankCustomers=new ArrayList<>();
     public CrudApi(){
         Creditcard creditcard1=new Creditcard();
@@ -22,6 +21,42 @@ public class CrudApi {
         Creditcard creditcard3=new Creditcard();
         creditcard3.setCardAvailable(20000);creditcard3.setCardNumber(98765454545454L);creditcard3.setCardPin(9999);
         myBankCustomers= Stream.of(creditcard1,creditcard2,creditcard3).collect(Collectors.toList());
+    }
+
+//    @DeleteMapping("/erase/{cardNumber}")
+//public String deleteByKey(@PathVariable long cardNumber){
+    @DeleteMapping("/erase")
+    public String deleteByKey(@RequestBody long cardNumber){
+        //myBankCustomers=myBankCustomers.stream().filter(each->each.getCardNumber()==cardNumber).collect(Collectors.toList());
+        Creditcard creditcard=null;
+        for(int index=0;index< myBankCustomers.size();index++){
+            if(myBankCustomers.get(index).getCardNumber()==cardNumber){
+                creditcard=myBankCustomers.get(index);
+                break;
+            }
+        }
+        if(creditcard!=null){
+            myBankCustomers.remove(creditcard);
+            return creditcard.getCardNumber()+" has been suspended";
+        }
+        return cardNumber+" hasn't matched with our records";
+    }
+
+    @PutMapping("/alter")
+    public Creditcard updateExists(@RequestBody Creditcard creditcard){
+        for(int index=0;index< myBankCustomers.size();index++){
+            if(myBankCustomers.get(index).getCardNumber()== creditcard.getCardNumber()){
+                myBankCustomers.set(index,creditcard);
+                return creditcard;
+            }
+        }
+        return null;
+    }
+
+    @PostMapping("/new")
+    public Creditcard saveNew(@RequestBody Creditcard creditcard){
+        myBankCustomers.add(creditcard);
+        return creditcard;
     }
 
     @GetMapping("/customers")

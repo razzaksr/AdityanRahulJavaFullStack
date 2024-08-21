34445c2e4d03889ffb114fcec51748857ba592db
razzaksr <razzaksr@gmail.com>
2024-08-21 19:54:16 +0530

    Spring boot web database entity created

--- SpringBoot-Controller/src/main/java/org/controller/demo/SpringBoot_Controller/Creditcard.java
+++ SpringBoot-Controller/src/main/java/org/controller/demo/SpringBoot_Controller/Creditcard.java
@@ -1,8 +1,19 @@
 package org.controller.demo.SpringBoot_Controller;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cardcredit")
 public class Creditcard {
+    @Id// primary key
+    @Column(name = "customer_id")
     private long cardNumber;
+    @Column(name = "available_limit")
     private int cardAvailable;
+    @Column(name = "secret_key")
     private int cardPin;
 
     public long getCardNumber() {
